c154a2395b9d65382e9880fb90d7270c1ba3f6d9
ACoolmanTelicent <a.coleman@telicent.io>
2024-10-10 13:28:12 +0100

    fix(errors): db codes used as http codes  - this is just some defensive code biasing for a minimal blast-radius

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -9,6 +9,9 @@ const logger = {
   info: (msg) => {
     console.log(`%c INFO: ${msg}`, "color: green");
   },
+  warn: (msg) => {
+    console.warn(`%c WARN: ${msg}`, "color: yellow");
+  },
   error: (msg) => {
     console.log(`%c ERROR: ${msg}`, "color: red");
   },
